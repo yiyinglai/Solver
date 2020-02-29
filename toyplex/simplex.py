cb3013a056168beefabe23549ddf1367b1728d5d
yiyinglai <57045546+yiyinglai@users.noreply.github.com>
2020-02-28 20:18:13 -0500

    Update simplex.py
    Removed add_var(), add_constr() interface, fell back to passing tab argument.
    Fixed mistake finding soln.

--- toyplex/simplex.py
+++ toyplex/simplex.py
@@ -1,5 +1,4 @@
 import numpy as np
-from toyplex.components import Var, LinExpr, LinConstr
 
 
 class Simplex:
@@ -8,67 +7,27 @@ class Simplex:
     It starts with a feasible basic solution, and moves from one feasible basic solution to
     another to decrease objective function, until reaching the minimum.
     """
-    def __init__(self):
+    def __init__(self, arg_tab, names=None):
         """Initiates a Simplex object."""
-        self.constrs = []
-        self.objective = None
-        self.tab = None
-        self.vars = {}
-        self.slacks = {}
-        self.n_constrs = 0
-        self.n_vars = 0
-
+        self.constrs = arg_tab[:-1, :]
+        self.objective = arg_tab[-1, :]
+        self.tab = np.vstack((self.constrs, self.objective))
+        self.n_constrs = self.constrs.shape[0]
+        self.n_vars = self.constrs.shape[1] - 1
+        self.names = names
         # code -1: solving, 0: solved, 1: unbounded, 2: infeasible
         self.code = -1
 
-    def add_var(self, type='continuous', name=None):
-        """Adds a decision variable."""
-        if name is None:
-            name = 'x' + str(int(len(self.vars))+1)
-        self.vars[name] = Var(name, type=type)
-        return self.vars[name]
-
-    def add_constr(self, constr: LinConstr):
-        """Adds a linear constraint."""
-        self.constrs.append(constr)
-
-    def set_tab(self):
-        """Sets tab."""
-        var_col = {}
-        for idx, key in enumerate(self.vars.keys()):
-            var_col[key] = idx
-        self.n_constrs = len(self.constrs)
-        self.n_vars = (len(self.vars) + len(self.slacks))
-        self.tab = np.zeros((self.n_constrs, self.n_vars + 1))
-        for idx, constr in enumerate(self.constrs):
-            for key in constr.coeffs.keys():
-                if constr.coeffs[key] != 0:
-                    self.tab[idx][var_col[key]] = constr.coeffs[key]
-                self.tab[idx][-1] = constr.b
-        self.tab = np.vstack((self.tab, self.objective))
-
-    def set_objective(self, objective: LinExpr):
-        """Sets objective."""
-        self.objective = np.zeros(len(self.vars) + len(self.slacks) + 1)
-
-        if 'const' in objective.coeffs.keys():
-            self.objective[-1] = -objective.coeffs['const']
-            del objective.coeffs['const']
-        else:
-            self.objective[-1] = 0
-
-        var_col = {}
-        for idx, key in enumerate(self.vars.keys()):
-            var_col[key] = idx
-        for key in objective.coeffs.keys():
-            if objective.coeffs[key] != 0:
-                self.objective[var_col[key]] = objective.coeffs[key]
-
     def print_tab(self, width=8):
         """Prints current tab."""
-        print(''.join(('x' + str(i + 1)).rjust(width) for i in range(self.tab.shape[1] - 1)))
-        for i in range(self.tab.shape[0]):
-            print(''.join('{0:0.3f}'.format(self.tab[i][j]).rjust(width) for j in range(self.tab.shape[1])))
+        if self.names is None:
+            print(''.join(('x' + str(i + 1)).rjust(width) for i in range(self.tab.shape[1] - 1)))
+            for i in range(self.tab.shape[0]):
+                print(''.join('{0:0.3f}'.format(self.tab[i][j]).rjust(width) for j in range(self.tab.shape[1])))
+        else:
+            print(''.join(name.rjust(width) for name in self.names))
+            for i in range(self.tab.shape[0]):
+                print(''.join('{0:0.3f}'.format(self.tab[i][j]).rjust(width) for j in range(self.tab.shape[1])))
 
     def indices(self):
         """Returns indices of basic vars in tab."""
@@ -106,7 +65,7 @@ class Simplex:
         n_art = self.n_constrs - len(indices)
 
         # artificial constr
-        art_constr = np.copy(self.tab[:self.n_constrs, :-1])
+        art_constr = np.copy(self.constrs[:, :-1])
         art_i_indices = []
         aux = np.zeros((self.n_constrs, n_art))
         art_j = 0
@@ -115,7 +74,7 @@ class Simplex:
                 aux[i][art_j] = 1
                 art_i_indices.append(i)
                 art_j += 1
-        art_constr = np.hstack((art_constr, aux, np.expand_dims(self.tab[:self.n_constrs, -1], axis=-1)))
+        art_constr = np.hstack((art_constr, aux, np.expand_dims(self.constrs[:, -1], axis=-1)))
 
         # artificial objective
         art_objective = np.zeros((1, art_constr.shape[1]))
@@ -187,9 +146,6 @@ class Simplex:
 
     def solve(self, verbose=False):
         """Solves linear program."""
-        # new
-        self.set_tab()
-
         # start solving
         print('original problem:')
         self.print_tab()
@@ -207,8 +163,10 @@ class Simplex:
         print('\nresult:')
         if self.code == 0:
             soln = np.zeros(self.n_vars)
-            for i in range(self.n_constrs):
-                soln[np.where(self.tab[i, :] == 1)] = self.tab[i, -1]
+            for i in range(self.n_vars):
+                arr = np.where(self.tab[:, i] == 1)[0]
+                if len(arr) == 1:
+                    soln[i] = self.tab[:, -1][arr[0]]
             print('solution:')
             print('(' + ', '.join('{0:0.3f}'.format(x) for x in soln) + ')')
             print('objective function:')
@@ -220,25 +178,35 @@ class Simplex:
 
 
 if __name__ == '__main__':
-    # new implementation with interface
-    m = Simplex()
-    x1 = m.add_var()
-    x2 = m.add_var()
-    x3 = m.add_var()
-    x4 = m.add_var()
-    x5 = m.add_var()
-    m.add_constr(x1 + 2 * x4 - x5 == 10)
-    m.add_constr(x2 - x4 - 5 * x5 == 20)
-    m.add_constr(x3 + 6 * x4 - 12 * x5 == 18)
-    m.set_objective(-2 * x4 + 3 * x5 - 60)
-    m.solve(verbose=True)
+    # in canonical form
+    tab1 = np.array([[1, 0, 0, 2, -1, 10],
+                    [0, 1, 0, -1, -5, 20],
+                    [0, 0, 1, 6, -12, 18],
+                    [0, 0, 0, -2, 3, 60]], dtype=float)
 
-    # m = Simplex()
-    # x1 = m.add_var()
-    # x2 = m.add_var()
-    # x3 = m.add_var()
-    # x4 = m.add_var()
-    # m.add_constr(x1 - 2 * x2 - 3 * x3 - 2 * x4 == 3)
-    # m.add_constr(x1 - x2 + 2 * x3 + x4 == 11)
-    # m.set_objective(2 * x1 - 3 * x2 + x3 + x4)
-    # m.solve(verbose=True)
+    tab2 = np.array([[3, 2, 0, 1, 0, 0, 60],
+                    [-1, 1, 4, 0, 1, 0, 10],
+                    [2, -2, 5, 0, 0, 1, 50],
+                    [-2, -3, -3, 0, 0, 0, 0]], dtype=float)
+
+    # unbounded
+    # tab3 = np.array([[],
+    #                  [],
+    #                  [],
+    #                  []], dtype=float)
+
+    # in standard form
+    std1 = np.array([[1, -2, -3, -2, 3],
+                    [1, -1, 2, 1, 11],
+                    [2, -3, 1, 1, 0]], dtype=float)
+    # infeasible
+    std2 = np.array([[1, 2, 1, 1, 0, 1],
+                    [-1, 0, 2, 0, -1, 4],
+                    [1, -1, 2, 0, 0, 4],
+                    [1, 1, 1, 0, 0, 0]], dtype=float)
+
+    # s = Simplex(tab1)
+    # s = Simplex(tab2, names=['a', 'b', 'c', 'd', 'e', 'f'])
+    s = Simplex(std1)
+    # s = Simplex(std2)
+    s.solve(verbose=True)
