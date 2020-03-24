43a62adcb26e54a19d12bea9bbda224d25be8d72
yiyinglai <57045546+yiyinglai@users.noreply.github.com>
2020-03-24 16:44:07 -0400

    Added callback functionality at integral solution,

--- toyplex/model.py
+++ toyplex/model.py
@@ -3,7 +3,6 @@ from toyplex.components import Var, LinExpr, LinConstr
 from toyplex.simplex import Simplex
 import math
 import copy
-__version__ = '0'
 
 
 class Node:
@@ -34,7 +33,7 @@ class Node:
         self.n_vars = 0
         self.n_constrs = 0
         # objective expression, objective array
-        self.objval = math.inf
+        self.objval = None
         self.objexpr = None
         self.objective = None
         # tab
@@ -65,8 +64,9 @@ class Node:
             self.vars.update(self.ints)
         return self.vars[name]
 
-    def add_constr(self, constr: LinConstr):
+    def add_constr(self, _constr: LinConstr):
         """Adds a linear constraint, and add slack and surplus variables as needed."""
+        constr = copy.deepcopy(_constr)
         # beautiful
         if constr.sense == '==':
             pass
@@ -84,7 +84,6 @@ class Node:
 
     def set_tab(self):
         """Sets tab."""
-        self.tab = None
         var_col = {}
         for idx, key in enumerate([*self.vars.keys()]+[*self.slacks.keys()]+[*self.surplus.keys()]):
             var_col[key] = idx
@@ -120,6 +119,9 @@ class Node:
             self.sense = sense
             self.objective = -self.objective
 
+        # set tab
+        self.set_tab()
+
     def describe(self):
         """Describes the linear program."""
         print('{}\t{}'.format(self.sense, self.objexpr))
@@ -131,8 +133,6 @@ class Node:
 
     def optimize(self, verbose=False):
         """Solves linear program."""
-        self.set_tab()
-
         # simplex algorithm
         names = [name for name in self.vars] + [name for name in self.slacks] + [name for name in self.surplus]
         self.spx = Simplex(self.tab, names=names)
@@ -167,6 +167,7 @@ class Model:
         self.candidates = {root.key: root}
         self.icmbkey = None
         self.icmbval = math.inf
+        self.strategy = 'best first'
 
         # default minimization
         self.sense = 'min'
@@ -181,6 +182,8 @@ class Model:
         self.surplus = {}
         # constraints
         self.constrs = []
+        self.use_lz_constrs = False
+        self.lz_constrs = []
         self.n_vars = 0
         self.n_constrs = 0
         # objective expression, objective array
@@ -188,6 +191,14 @@ class Model:
         self.objval = math.inf
         self.objexpr = None
         self.objective = None
+
+        # callback functions
+        self.int_cb = None
+        self.frac_cb = None
+        self.lz_parent = None
+        self.next_node = None
+
+        self.verbose = False
 
     def add_var(self, type='cont', lb=None, ub=None, name=None):
         """Adds a decision variable."""
@@ -200,12 +211,12 @@ class Model:
 
         # decision vars
         self.vars.update(self.nodes[0].vars)
-        self.conts.update(self.nodes[0].vars)
-        self.bins.update(self.nodes[0].vars)
-        self.ints.update(self.nodes[0].vars)
+        self.conts.update(self.nodes[0].conts)
+        self.bins.update(self.nodes[0].bins)
+        self.ints.update(self.nodes[0].ints)
         # non-decision vars
-        self.slacks.update(self.nodes[0].vars)
-        self.surplus.update(self.nodes[0].vars)
+        self.slacks.update(self.nodes[0].slacks)
+        self.surplus.update(self.nodes[0].surplus)
         # update model constrs
         self.constrs = self.nodes[0].constrs[:]
         return self.nodes[0].vars[name]
@@ -214,6 +225,24 @@ class Model:
         """Adds a linear constraint, and add slack and surplus variables as needed."""
         self.nodes[0].add_constr(constr)
         self.constrs = self.nodes[0].constrs[:]
+
+    def add_lz_constr(self, lz_constr: LinConstr):
+        """Adds a lazy linear constraint, and add slack and surplus variables as needed."""
+        self.lz_constrs.append(lz_constr)
+        # add lazy constraint to leave nodes
+        for node in self.candidates.values():
+            node.add_constr(lz_constr)
+            node.set_objective(self.objexpr, sense=self.sense)
+        # make child node
+        child_node = copy.deepcopy(self.lz_parent)
+        child_node.add_constr(lz_constr)
+        self.lz_parent = None
+        child_node.key = len(self.nodes)
+        child_node.code = -1
+        child_node.set_objective(self.objexpr, sense=self.sense)
+        self.nodes[child_node.key] = child_node
+        # promote child_node in candidates
+        self.next_node = child_node
 
     def set_objective(self, objexpr: LinExpr, sense='min'):
         """Sets objective."""
@@ -226,84 +255,129 @@ class Model:
 
     def describe(self):
         """Describes the linear program."""
-        print('{}\t{}'.format(self.sense, self.objexpr))
+        print('\n{}\t{}'.format(self.sense, self.objexpr))
         for idx, constr in enumerate(self.constrs):
             if idx == 0:
                 print('st\t{}'.format(constr))
             else:
                 print('\t{}'.format(constr))
+        if self.conts:
+            print('continuous: ' + ', '.join(str(var) for var in self.conts))
+        if self.ints:
+            print('integral: ' + ', '.join(str(var) for var in self.ints))
+        if self.bins:
+            print('binary: ' + ', '.join(str(var) for var in self.bins))
+        print()
 
     def candidates_queue(self):
         """Returns a queue of candidate nodes in the order of processing."""
-        return [*self.candidates]
+        if self.strategy == 'best first':
+            if self.sense == 'min':
+                return sorted(self.candidates, key=lambda key: self.candidates[key].objval if self.candidates[key].objval is not None else math.inf)
+            else:
+                return sorted(self.candidates, key=lambda key: self.candidates[key].objval if self.candidates[key].objval is not None else -math.inf, reverse=True)
 
-    def optimize(self, verbose=False):
-        """Optimizes the mixed integer programming model."""
-        while self.code == -1:
-            if self.candidates_queue():
-                key = self.candidates_queue()[0]
-                node = self.nodes[key]
-                node.optimize(verbose=verbose)
+    def update_icmb(self, node):
+        """Updates incumbent key and objective value as needed."""
+        if (self.sense == 'max' and node.objval > self.icmbval) or (
+                self.sense == 'min' and node.objval < self.icmbval):
+            self.icmbkey = node.key
+            self.icmbval = node.objval
 
-                # print('\nNode {}'.format(node.key))
-                # node.describe()
-                # if node.code == 0:
-                #     print(', '.join(str(node.vars[key].val) for key in self.vars.keys()))
-                #     print('Objval: {}'.format(node.objval))
-                # else:
-                #     print(node.code)
+    def process(self, key):
+        """Processes a node."""
+        node = self.nodes[key]
+        if node.code == 0:
+            # branch or bound
+            frac_vars = [*node.ints.values()] + [*node.bins.values()]
+            var_opts = []
+            for var in frac_vars:
+                if not float(var.val).is_integer():
+                    var_opts.append(var)
 
-                # found a solution
-                if node.code == 0:
-                    # branch or bound
-                    frac_vars = [*node.ints.values()] + [*node.bins.values()]
-                    var_opts = []
-                    for var in frac_vars:
-                        if not float(var.val).is_integer():
-                            var_opts.append(var)
-
-                    # fractional solution
-                    if var_opts:
-                        # branch
-                        if (self.sense == 'max' and node.objval > self.icmbval) or (self.sense == 'min' and node.objval < self.icmbval):
-                            del self.candidates[key]
-                            var = var_opts[0]
-                            # branch down
-                            left_node = copy.deepcopy(node)
-                            left_node.key = len(self.nodes)
-                            left_node.code = -1
-                            left_node.add_constr(var <= math.floor(var.val))
-                            left_node.set_objective(self.objexpr, sense=self.sense)
-                            self.nodes[left_node.key] = left_node
-                            self.candidates[left_node.key] = left_node
-                            # branch up
-                            right_node = copy.deepcopy(node)
-                            right_node.key = len(self.nodes)
-                            right_node.code = -1
-                            right_node.add_constr(var >= math.ceil(var.val))
-                            right_node.set_objective(self.objexpr, sense=self.sense)
-                            self.nodes[right_node.key] = right_node
-                            self.candidates[right_node.key] = right_node
-                        # bound
-                        else:
-                            del self.candidates[key]
-                    # integral solution
-                    else:
-                        del self.candidates[key]
-                        # update incumbent value
-                        if (self.sense == 'max' and node.objval > self.icmbval) or (self.sense == 'min' and node.objval < self.icmbval):
-                            self.icmbkey = node.key
-                            self.icmbval = node.objval
-
-                # unbounded: terminate optimization
-                elif node.code == 1:
-                    self.code = 1
-                # infeasible:
-                elif node.code == 2:
+            # fractional solution
+            if var_opts:
+                # branch
+                if (self.sense == 'max' and node.objval > self.icmbval) or (
+                        self.sense == 'min' and node.objval < self.icmbval):
+                    var = var_opts[0]
+                    # branch down
+                    left_node = copy.deepcopy(node)
+                    left_node.key = len(self.nodes)
+                    left_node.code = -1
+                    left_node.add_constr(var <= math.floor(var.val))
+                    left_node.set_objective(self.objexpr, sense=self.sense)
+                    self.nodes[left_node.key] = left_node
+                    self.candidates[left_node.key] = left_node
+                    # branch up
+                    right_node = copy.deepcopy(node)
+                    right_node.key = len(self.nodes)
+                    right_node.code = -1
+                    right_node.add_constr(var >= math.ceil(var.val))
+                    right_node.set_objective(self.objexpr, sense=self.sense)
+                    self.nodes[right_node.key] = right_node
+                    self.candidates[right_node.key] = right_node
                     del self.candidates[key]
+                # bound
+                else:
+                    del self.candidates[key]
+
+            # integral solution
+            else:
+                # no callback function
+                if self.int_cb is None:
+                    del self.candidates[key]
+                    self.update_icmb(node)
+                else:
+                    del self.candidates[key]
+                    self.lz_parent = node
+                    self.int_cb(self)
+                    # no lazy constraint is added
+                    if self.lz_parent:
+                        self.lz_parent = None
+                        self.update_icmb(node)
+
+        # unbounded: terminate optimization
+        elif node.code == 1:
+            self.code = 1
+        # infeasible:
+        elif node.code == 2:
+            del self.candidates[key]
+
+    def optimize(self, int_cb=None, frac_cb=None, verbose=False):
+        """Optimizes the mixed integer programming model."""
+        if int_cb is not None:
+            self.int_cb = int_cb
+        if frac_cb is not None:
+            self.frac_cb = frac_cb
+        self.verbose = verbose
+
+        while self.code == -1:
+            if self.candidates or self.next_node:
+                if self.next_node:
+                    key = self.next_node.key
+                    node = self.next_node
+                    self.candidates[key] = node
+                    self.next_node = None
+                else:
+                    key = self.candidates_queue()[0]
+                    node = self.nodes[key]
+
+                node.optimize(verbose=self.verbose)
+                if self.verbose:
+                    print('\nNode {}'.format(node.key))
+                    node.describe()
+                    if node.code == 0:
+                        print(', '.join(str(node.vars[key].val) for key in self.vars.keys()))
+                        print('Objval: {}'.format(node.objval))
+                    else:
+                        print(node.code)
+                self.process(key)
+
             # no more candidates
             else:
                 self.code = 0
+
         # finishing
         if self.code == 0:
             self.objkey = self.icmbkey
@@ -313,17 +387,28 @@ class Model:
             print('\nOptimal objective value: {}'.format(self.objval))
         elif self.code == 1:
             print('Model unbounded')
+        elif self.code == 2:
+            print('Model infeasible')
 
 
 if __name__ == '__main__':
+    def my_integral_callback(model):
+        print('Inside my integral callback')
+        x_val = model.lz_parent.vars['x'].val
+        y_val = model.lz_parent.vars['y'].val
+        if 4 * x_val + y_val > 36.5:
+            print('Add lazy constraint', str(4 * x + y <= 36.5))
+            model.add_lz_constr(4 * x + y <= 36.5)
+
     m = Model()
     x = m.add_var(type='int', name='x')
     y = m.add_var(type='bin', name='y')
     m.add_constr(3*x + 5*y <= 78.8)
-    m.add_constr(4*x + y <= 36.5)
+    # m.add_constr(4*x + y <= 36.5)
     m.set_objective(5*x + 4*y, sense='max')
     m.describe()
-    m.optimize(True)
+    m.strategy = 'best first'
+    m.optimize(int_cb=my_integral_callback, verbose=True)
 
     if m.code == 0:
         print('\nObjective value:{}'.format(m.objval))
